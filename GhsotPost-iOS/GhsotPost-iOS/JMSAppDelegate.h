836e165e902e1f64c99b7220e57230df7ed2d0b5
Jared Sorge <jared@jsorge.net>
2014-03-25 07:10:41 -0700

    Initial commit

---
+++ GhsotPost-iOS/GhsotPost-iOS/JMSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JMSAppDelegate.h
+//  GhsotPost-iOS
+//
+//  Created by Jared Sorge on 3/25/14.
+//  Copyright (c) 2014 jsorge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JMSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GhsotPost-iOS/GhsotPost-iOS/JMSViewController.h
@@ -0,0 +1,13 @@
+//
+//  JMSViewController.h
+//  GhsotPost-iOS
+//
+//  Created by Jared Sorge on 3/25/14.
+//  Copyright (c) 2014 jsorge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JMSViewController : UIViewController
+
+@end
